da3d2778bbac6c21f61dd719a6dc3bdb6db1fdf5
Yousefallam221b <yousef.allam221b@gmail.com>
2021-12-09 23:06:43 +0200

    could be useful insertations

--- Insertions.sql
+++ Insertions.sql
@@ -7,7 +7,22 @@ DBCC CHECKIDENT (PostGradUser, RESEED, 0); -- reseeds the entity starting from 1
 INSERT INTO PostGradUser VALUES ('Admin email', 'Admin Password');
 INSERT INTO Admin VALUES (SCOPE_IDENTITY());
 
--- insert Students
+-- insert Students 4 gucian, 3 nongucian, 2 supervisors, 2 examiners, 2 admin
+EXEC StudentRegister 'Harry', 'Potter', 'HarryPotterPass', 'Gryffindor', 1, 'HarryPotterPassEmail ', '4 Privet Drive';
+EXEC StudentRegister 'Ronald', 'Weasley', 'RonaldWeasleyPass', 'Gryffindor', 1, 'RonaldWeasleyPassEmail', 'The Burrow';
+EXEC StudentRegister 'Hermione', 'Granger', 'HermioneGrangerPass', 'Gryffindor', 1, 'HermioneGrangerEmail', 'Hampstead Garden Suburb';
+EXEC StudentRegister 'Neville ', 'Longbottom ', 'NevilleLongbottomPass', 'Gryffindor', 1, 'NevilleLongbottomEmail', 'The Leaky Cauldron';
+
+EXEC StudentRegister 'Draco', 'Malfoy', 'DracoMalfoyPass', 'Slytherin', 0, 'DracoMalfoyEmail', ' The Malfoy Mansion';
+EXEC StudentRegister 'Cedric', 'Diggory', 'CedricDiggoryPass', 'Hufflepuff', 0, 'CedricDiggoryEmail', 'Ottery St Catchpole';
+EXEC StudentRegister 'Luna', 'Lovegood ', 'LunaLovegoodPass', 'Ravenclaw', 0, 'LunaLovegoodEmail	', 'The Lovegood House';
+
+EXEC StudentRegister 'stu first_name 1', 'gucian last_name 1', 'gucian password 1', 'gucian faculty 1', 1, 'gucian email 1', 'gucian adrs 1';
+EXEC StudentRegister 'stu first_name 1', 'gucian last_name 1', 'gucian password 1', 'gucian faculty 1', 1, 'gucian email 1', 'gucian adrs 1';
+EXEC StudentRegister 'stu first_name 1', 'gucian last_name 1', 'gucian password 1', 'gucian faculty 1', 1, 'gucian email 1', 'gucian adrs 1';
+
+EXEC StudentRegister 'stu first_name 1', 'gucian last_name 1', 'gucian password 1', 'gucian faculty 1', 1, 'gucian email 1', 'gucian adrs 1';
+EXEC StudentRegister 'stu first_name 1', 'gucian last_name 1', 'gucian password 1', 'gucian faculty 1', 1, 'gucian email 1', 'gucian adrs 1';
 EXEC StudentRegister 'stu first_name 1', 'gucian last_name 1', 'gucian password 1', 'gucian faculty 1', 1, 'gucian email 1', 'gucian adrs 1';
 
 -- insert Supervisors
